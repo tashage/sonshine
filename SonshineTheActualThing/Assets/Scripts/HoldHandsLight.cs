81ad5996035e143d89b0e7cbf9a3d09e85ac8004
tashage <tashage@gmail.com>
2014-08-20 12:04:14 +1000

    fixin lights

--- SonshineTheActualThing/Assets/Scripts/HoldHandsLight.cs
+++ SonshineTheActualThing/Assets/Scripts/HoldHandsLight.cs
@@ -37,8 +37,29 @@ public class HoldHandsLight : MonoBehaviour {
             }
         }
         //Debug.Log("light intensity = " + kidLight.intensity);
+
+        //try range instead of intensity....
+        //kidLight.intensity = 0.9f;
+        //if (bTethered)
+        //{
+        //    //Debug.Log("tethered!");
+        //    if (kidLight.range < 50)//(kidLight.intensity < 0.9f)
+        //    {
+        //        kidLight.range += 1;//kidLight.intensity += 0.05f;
+        //    }
+        //}
+        //else
+        //{
+        //    if (kidLight.range > 20)//(kidLight.intensity > 0.2f)
+        //    {
+        //        kidLight.range -= 1;//kidLight.intensity -= 0.02f;
+        //    }
+        //}
 	}
 
+
+    //I guess tethered will be an agent bool, and this function will use that value
+    //in the meantime, "tether" will just be a collision"
     void OnTriggerEnter(Collider other)
     {
         if (other == playerCollider)
