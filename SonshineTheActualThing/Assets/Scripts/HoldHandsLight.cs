cadcb29b6ed5278ab9a1c3a4cea82407b4bcbb84
tashage <tashage@gmail.com>
2014-08-20 10:24:16 +1000

    integrating watson AI

---
+++ SonshineTheActualThing/Assets/Scripts/HoldHandsLight.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Author: Tash
+/// Date Created: 18/8/14
+/// Last Updated: 20/8/14
+/// Description:
+/// class turns light on when player and Watson collide (will be when holding hands)
+/// </summary>
+public class HoldHandsLight : MonoBehaviour {
+
+    public Light kidLight;
+    public Collider playerCollider;
+
+    bool bTethered = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (bTethered)
+        {
+            //Debug.Log("tethered!");
+            if (kidLight.intensity < 0.9f)
+            {
+                kidLight.intensity += 0.05f;
+            }
+        }
+        else
+        {
+            if (kidLight.intensity > 0.2f)
+            {
+                kidLight.intensity -= 0.02f;
+            }
+        }
+        //Debug.Log("light intensity = " + kidLight.intensity);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other == playerCollider)
+        {
+            bTethered = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other == playerCollider)
+        {
+            bTethered = false;
+        }
+    }
+}
