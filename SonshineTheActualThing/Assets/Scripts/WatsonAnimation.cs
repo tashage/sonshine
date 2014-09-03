b8e1881c0289f9915e9240c4386e008b1ae5fffa
tashage <tashage@gmail.com>
2014-09-03 10:43:26 +1000

    working on mecanim, almost functioning

---
+++ SonshineTheActualThing/Assets/Scripts/WatsonAnimation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WatsonAnimation : MonoBehaviour {
+
+    public Animator WatsonAnimator; //reference the animator
+    private bool LMBDown, RMBDown; 
+
+	// Use this for initialization
+	void Start () {
+        LMBDown = false;
+        RMBDown = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //mouse buttons: left = 0, right = 1, middle = 2
+        //check if left button is pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            LMBDown = true;
+        }
+        else
+        {
+            LMBDown = false;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            RMBDown = true;
+        }
+        else
+        {
+            RMBDown = false;
+        }
+	}
+}
