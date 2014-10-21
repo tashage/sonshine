69e4bae8ed44a634904fd3c833c91a53a3286945
akurt5 <anton.n.kurtz@gmail.com.au>
2014-10-21 11:21:40 +1100

    couple of small changes

--- SonshineTheActualThing/Assets/Scripts/States/MenuManager.cs
+++ SonshineTheActualThing/Assets/Scripts/States/MenuManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SceneManager : MonoBehaviour 
+public class MenuManager : MonoBehaviour 
 {
 
     public  GameObject FirstState;//States
