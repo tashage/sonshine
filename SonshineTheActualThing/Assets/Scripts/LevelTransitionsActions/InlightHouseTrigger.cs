0b48cac2e2ef0cb9354380afba2daf8445a735a7
unknown <cgbocaj@gmail.com>
2014-11-03 10:31:26 +1100

    Adding in light house trigger

---
+++ SonshineTheActualThing/Assets/Scripts/LevelTransitionsActions/InlightHouseTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Author: Jacob Connelly
+/// Date Created: 3/11/2014
+/// Last Updated: 3/11/2014
+/// Description: 
+/// This script will be used to shine a spotlight at the boat when watson is in the top of the light house,
+/// this is intended to be attached to something with a collision box.
+/// </summary>
+
+public class InlightHouseTrigger : MonoBehaviour {
+
+    bool bSpotlightOn = false;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
