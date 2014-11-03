30b84e72c6586db155f04b96409af0940e90e81f
akurt5 <anton.n.kurtz@gmail.com.au>
2014-11-03 17:38:25 +1100

    Teleporter working and stuff.

---
+++ SonshineTheActualThing/Assets/Scripts/Environment/Teleporter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour 
+{
+
+	void OnTriggerEnter()
+	{
+		this.SendMessageUpwards("TeleTrigger");
+	}
+	void OnTriggerExit()
+	{
+		this.SendMessageUpwards("TeleExit");
+	}
+}
