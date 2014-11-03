1309778731a144d825246b8436073a9e8cc772dd
patriotpidgeon <cgbocaj@gmail.com>
2014-11-04 01:30:57 +1100

    finished music

--- SonshineTheActualThing/Assets/Scripts/MusicHandler.cs
+++ SonshineTheActualThing/Assets/Scripts/MusicHandler.cs
@@ -5,9 +5,11 @@ public class MusicHandler : MonoBehaviour {
 
     
     bool seenChild =false;
-    float fDistanceForSeen = 40;
+    float fDistanceForSeen = 20;
     bool bHeldHandsWithsChild = false;
-    bool AteFruit = false;
+    bool bAteFruit = false;
+    bool bNearEnd = false;
+    bool bNearEndGame = false;
 
     bool bFade = false;
     bool bFadeFinisehd = false;
@@ -15,6 +17,10 @@ public class MusicHandler : MonoBehaviour {
     public GameObject goPlayer;
 
     public GameObject goChild;
+
+    public GameObject goEndPoint;
+
+    public GameObject goEndPointGame;
 
     public AudioClip acIntro;
     public AudioClip acLoop1;
@@ -25,6 +31,8 @@ public class MusicHandler : MonoBehaviour {
     public AudioClip acSegue2;
     public AudioClip acFinale;
 
+    int iLoopPoint;
+    AudioClip acLoopPoint;
     AudioClip acNextToPlay;
 
     AudioSource thisAudioSource;
@@ -34,6 +42,7 @@ public class MusicHandler : MonoBehaviour {
         thisAudioSource.clip = acIntro;
         acNextToPlay = acLoop1;
         thisAudioSource.Play();
+        iLoopPoint = 1;
 	}
 	
 	// Update is called once per frame
@@ -41,50 +50,85 @@ public class MusicHandler : MonoBehaviour {
     {
 
         CheckGameStatus();
-
-        // when the change over happens
-       /* if (thisAudioSource.isPlaying == false)
-        {
-            if (seenChild)
-            {
-                if (bHeldHandsWithsChild)
-                {
-                    acNextToPlay = acSegue1;
-                    Debug.Log("next to play" + acNextToPlay.name);
-                }
-                else
-                {
-                    acNextToPlay = acLoop1;
-                    Debug.Log("next to play" + acNextToPlay.name);
-                }
-            }
-            thisAudioSource.clip = acNextToPlay;
-            thisAudioSource.Play();
-            Debug.Log("Playing" + thisAudioSource.clip.name);
-        }*/
+        FadeToNext();
+        CheckLoopPoint();
+       
 	}
 
     // this checks the neccesary
     void CheckGameStatus()
     {
         if(!seenChild)
-        if (Vector3.Distance(goPlayer.transform.position, goChild.transform.position) < fDistanceForSeen)
-        {
-            seenChild = true;
-            acNextToPlay = acSegue1;
+            if (Vector3.Distance(goPlayer.transform.position, goChild.transform.position) < fDistanceForSeen)
+            {
+                seenChild = true;
+                acNextToPlay = acSegue1;
+                SetFade();
+                iLoopPoint = 2;
             
-            thisAudioSource.Play();
-            Debug.Log("seen child");
-        }
+               
+            }
 
         if(!bHeldHandsWithsChild)
-        if (goChild.GetComponent<Watson>().bTethered)
+            if (goChild.GetComponent<Watson>().bTethered)
+            {
+                bHeldHandsWithsChild = true;
+                
+                acNextToPlay = acLoop2;
+                iLoopPoint = 3;
+            }
+       
+        if(!bNearEnd)
+            if(Vector3.Distance(goPlayer.transform.position, goEndPoint.transform.position) < 10.0f)
+            {
+                bNearEnd=true;
+                iLoopPoint = 4;
+            }
+        if (!bNearEndGame && goEndPointGame!=null)
+            if (Vector3.Distance(goPlayer.transform.position, goEndPointGame.transform.position) < 50.0f)
+            {
+
+                bNearEndGame = true;
+                acNextToPlay = acFinale;
+                SetFade();
+                iLoopPoint = 4;
+                // close to finish
+            }
+
+        if(!thisAudioSource.isPlaying)
         {
-            bHeldHandsWithsChild = true;
-            Debug.Log("HeldHands");
+            thisAudioSource.clip = acLoopPoint;
+            thisAudioSource.Play();
         }
 
     }
+
+    void CheckLoopPoint()
+    {
+        if (iLoopPoint == 1)
+        {
+            acLoopPoint = acLoop1;
+        }
+        else 
+        if (iLoopPoint == 2)
+        {
+            acLoopPoint = acLoop2;
+        }
+        else
+            
+        if (iLoopPoint == 3)
+        {
+            acLoopPoint = acLoop3;
+        }
+        else
+            
+        if (iLoopPoint == 4)
+        {
+            acLoopPoint = acLoop4;
+        }
+        
+    }
+
     void SetFade()
     {
         bFade = true;
@@ -94,12 +138,30 @@ public class MusicHandler : MonoBehaviour {
     {
         if (bFade)
         {
-            thisAudioSource.volume -= Time.deltaTime / 10;
+            thisAudioSource.volume -= Time.deltaTime ;
+           
         }
+        else 
+        {
+            if (thisAudioSource.volume < 0.5)
+            {
+                thisAudioSource.volume += Time.deltaTime;
+               
+            }
+            if (thisAudioSource.volume > 0.5)
+            {
+                thisAudioSource.volume = 0.5f;
+            }
+        }
+
         if (thisAudioSource.volume <= 0)
         {
             bFade = false;
             bFadeFinisehd = true;
+            thisAudioSource.clip = acNextToPlay;
+            thisAudioSource.Play();
+
         }
+       
     }
 }
